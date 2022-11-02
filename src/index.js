dec830a4614b5d41c1fa576795bd40bbc5c2b644
gkumar-pandey <gautamshekhar078@gmail.com>
2022-11-02 12:51:35 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "antd-notifications-messages/lib/styles/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
